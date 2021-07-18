06590d7fb992f2ef0b2ddf9e1f0b00042f86c0a5
Edgar Olivar <edgarolivar16@gmail.com>
2021-07-18 14:36:45 -0400

    ✨ first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# MS client with Nest framework
+
+- Basic example about a microservices client
+
+`Note:` this app require a rabbit instance
+
+This examples are part from the [Guide to Nest JS-RabbitMQ Microservices](https://medium.com/swlh/guide-to-nest-js-rabbitmq-microservices-e1e8655d2853)
+
+This example require an [App Consumer](https://github.com/The3dgar/nest_ms-consumer)
+
+E.O
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@localhost:5672/hello'],
+      queue: 'user-messages',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  app.listen(() => console.log('MS-APP listening'));
+}
+bootstrap();
